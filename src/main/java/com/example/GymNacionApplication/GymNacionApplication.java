60c0b437df9bb08f26620e02b6896b6d7edb43b6
Jesús <jedatoos@gmail.com>
2026-02-08 20:33:46 -0500

    quite crecion de class CorsConfig del inicio del programa

--- src/main/java/com/example/GymNacionApplication/GymNacionApplication.java
+++ src/main/java/com/example/GymNacionApplication/GymNacionApplication.java
@@ -12,15 +12,5 @@ public class GymNacionApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GymNacionApplication.class, args);
 	}
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**") // todas las rutas
-						.allowedOrigins("*") // permite cualquier origen, o tu frontend
-						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
-			}
-		};
-	}
+
 }
